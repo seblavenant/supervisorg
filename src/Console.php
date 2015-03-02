e221e5cb59682f38b19864ef60edc2c599d76ff1
Claude LE BRIS <claude.lebris@gmail.com>
2015-03-02 21:54:46 +0100

    Initial commit

---
+++ src/Console.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Supermonitord;
+
+use Puzzle\Configuration;
+use Spear\Silex\Provider\Commands\AsseticDumper;
+
+class Console
+{
+    private
+        $app,
+        $configuration;
+
+    public function __construct(Application $dic)
+    {
+        $this->configuration = $dic['configuration'];
+
+        $this->app = new \Symfony\Component\Console\Application('silex-spear-app');
+
+        $this->app->add(new Commands\GreetCommand());
+        $this->app->add(new AsseticDumper($this->configuration, $dic['assetic.dumper'], $dic['assetic.path_to_web']));
+    }
+
+    public function run()
+    {
+        $this->app->run();
+    }
+}
---
+++ www/index.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Puzzle\Configuration\Yaml;
+use Gaufrette\Filesystem;
+use Gaufrette\Adapter\Local;
+use Supermonitord\Application;
+
+$configurationFilesStorage = new Filesystem(new Local(__DIR__ . '/../config'));
+$configuration = new Yaml($configurationFilesStorage);
+
+$rootDir = realpath(__DIR__ . '/..');
+
+$app = new Application($configuration, $rootDir);
+
+$app->run();
