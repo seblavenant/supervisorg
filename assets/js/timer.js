173cced86e57e0de585eb15d76cb9581aa0deda0
Claude LE BRIS <claude.lebris@gmail.com>
2016-03-16 23:09:25 +0100

    feat: add timer to reload the page

---
+++ assets/js/timer.js
@@ -0,0 +1,32 @@
+$countdown = $("#timer");
+
+var countdown = $countdown.countdown360({
+    radius: 18,
+    seconds: 10,
+    label: [],
+    fillStyle: "transparent",
+    fontSize: 18,
+    fontColor: '#FFFFFF',
+    strokeStyle: '#FFFFFF',
+    autostart: false,
+    onComplete: function () {
+        window.location.reload(false);
+    }
+});
+
+countdown.start();
+$playButton = $('#play');
+$stopButton = $('#stop');
+$('#stop').click(function() {
+    countdown.stop();
+    $stopButton.hide();
+    $playButton.show();
+    return false;
+});
+
+$('#play').click(function() {
+    countdown.start();
+    $stopButton.show();
+    $playButton.hide();
+    return false;
+});
