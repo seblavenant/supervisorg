39993b5c9f6c895c4690b85f8bb80e61c1acf81f
Niktux <nico.ln@gmail.com>
2016-04-02 14:01:23 +0200

    ui: manage persistant view choices

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,35 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
+  
+  var condensedViewStorageKey = 'view.condensed';
+  
+  var toggleView = function () {
+	  $('.small-box').toggleClass('condensedView');
+  };
+  
+  $('#toggleView').click(function() {
+	  if(localStorage)
+	  {
+		  var view = localStorage.getItem(condensedViewStorageKey);
+		  if(view === "enabled")
+		  {
+			  localStorage.setItem(condensedViewStorageKey, "disabled");
+		  }
+		  else
+		  {
+			  localStorage.setItem(condensedViewStorageKey, "enabled");
+		  }
+	  }
+	  
+	  toggleView();
+  });
+  
+  if(localStorage)
+  {
+	  var view = localStorage.getItem(condensedViewStorageKey);
+	  if(view === "enabled")
+	  {
+		  toggleView();
+	  }
+  }
 })
